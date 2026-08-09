af6b0574f44dff9f9e3eb2fe2abf6fbc0c6991d5
lvinist <alvin.geomatics@gmail.com>
2026-08-10 03:17:33 +0800

    chore(ci): fix compileSdk override using plugins.withId instead of afterEvaluate

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,11 +19,14 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 subprojects {
-    afterEvaluate {
-        if (plugins.hasPlugin("com.android.library") || plugins.hasPlugin("com.android.application")) {
-            extensions.configure<com.android.build.gradle.BaseExtension> {
-                compileSdkVersion(36)
-            }
+    plugins.withId("com.android.library") {
+        extensions.configure<com.android.build.gradle.LibraryExtension> {
+            compileSdk = 36
+        }
+    }
+    plugins.withId("com.android.application") {
+        extensions.configure<com.android.build.gradle.AppExtension> {
+            compileSdkVersion(36)
         }
     }
 }
