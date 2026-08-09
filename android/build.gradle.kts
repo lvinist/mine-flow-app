e552d4076fcf7199da1a5a15fa15b2de7d564c5c
lvinist <alvin.geomatics@gmail.com>
2026-08-10 03:34:54 +0800

    chore(ci): upgrade file_picker to ^9.0.0 to fix compileSdk 36 requirement

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -18,18 +18,6 @@ subprojects {
 subprojects {
     project.evaluationDependsOn(":app")
 }
-subprojects {
-    plugins.withId("com.android.library") {
-        extensions.configure<com.android.build.gradle.LibraryExtension> {
-            compileSdk = 36
-        }
-    }
-    plugins.withId("com.android.application") {
-        extensions.configure<com.android.build.gradle.AppExtension> {
-            compileSdkVersion(36)
-        }
-    }
-}
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
