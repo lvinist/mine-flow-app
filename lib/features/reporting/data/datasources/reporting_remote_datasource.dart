2e40d58a08e72514bd865007dd0dfc9f1bf8edfb
lvinist <alvin.geomatics@gmail.com>
2026-08-29 20:22:59 +0700

    Fix users table fkey ambiguity in reporting query

--- lib/features/reporting/data/datasources/reporting_remote_datasource.dart
+++ lib/features/reporting/data/datasources/reporting_remote_datasource.dart
@@ -22,7 +22,7 @@ class ReportingRemoteDataSource {
     try {
       final response = await supabaseClient
           .from('attendance_records')
-          .select('*, users!inner(name)')
+          .select('*, users!attendance_records_user_id_fkey!inner(name)')
           .eq('site_id', siteId)
           .gte('date', startDate.toIso8601String())
           .lte('date', endDate.toIso8601String())
