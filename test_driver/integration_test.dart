90a995c31a3a78b2f6aa644f63f203d004cc9bf2
lvinist <alvin.geomatics@gmail.com>
2026-08-31 01:23:25 +0700

    fix(STEP-48.15): use extended integration driver for screenshots

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -1,10 +1,17 @@
 import 'dart:io';
-import 'package:integration_test/integration_test_driver.dart';
+import 'package:integration_test/integration_test_driver_extended.dart';
 
 Future<void> main() => integrationDriver(
-      onScreenshot: (String screenshotName, List<int> screenshotBytes, [Map<String, Object?>? args]) async {
-        final File image = await File('../mine-flow-docs/reports/design-review/step-0048/$screenshotName.png').create(recursive: true);
+  onScreenshot:
+      (
+        String screenshotName,
+        List<int> screenshotBytes, [
+        Map<String, Object?>? args,
+      ]) async {
+        final File image = await File(
+          '../mine-flow-docs/reports/design-review/step-0048/$screenshotName.png',
+        ).create(recursive: true);
         image.writeAsBytesSync(screenshotBytes);
         return true;
       },
-    );
+);
